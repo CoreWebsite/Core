5cb36fae1c0540320b1e41d666d5b9920f32c457
vitx8 <andreasboy97@hotmail.com>
2017-03-25 01:45:38 +0100

    Patch 0.1
    Patch 0.1
    - Index php [added]
    - style css [added]

---
+++ index.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+<title></title>
+
+<link rel="stylesheet" type="text/css" href="Style/main.css" />
+<link rel="stylesheet" type="text/css" href="style/navigation.css"  />
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+
+</head>
+<body> 
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">LINK</a></li>
+        <li><a href="#">LINK</a></li>
+        <li><a href="#">LINK</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<div class="col-sm-10 text-center"> 
+      <h1>Welcome TEST col 10</h1>
+      <p></p>
+      <hr>
+      <h3></h3>
+      <p></p>
+</div>
+
+
+<div class="col-sm-2 text-left"> 
+      <h1>Welcome col 2</h1>
+      <p></p>
+      <hr>
+      <h3></h3>
+      <p></p>
+</div>
+
+<footer class="container-fluid text-center">
+  <p>Footer Text </p>
+</footer>
+ 
+      
+      
+
+
+
+</body>
+</html>
