275ee6f53464f4b29c01205a460455ac41072f88
vitx8 <andreasboy97@hotmail.com>
2017-03-29 01:07:45 +0200

    adding a new php about us new profile
    Profile images added

---
+++ aboutus.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+  <title>test</title>
+  <link rel="icon" type="image/png" href="image/logoicon.png" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href=   "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="javascript/jquary.js"> </script>
+  <link rel="stylesheet" href="style/main.css" type="text/css">
+  <link rel="stylesheet" href="style/navigation.css" type="text/css">
+  <link rel="stylesheet" href="style/aboutus.css" type="text/css">
+
+  
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="navbar-default" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="about.php">About Us</a></li>
+        <li><a href="tutorials.php">Tutorials</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#" onclick="document.getElementById('id01').style.display='block'"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+<div class="col-sm-2 text-left">
+      <h1>Welcome col 2</h1>
+      <p></p>
+      <hr>
+      <h3></h3>
+      <p></p>
+</div>
+
+<div class="col-sm-10">
+
+	<div class="container_profile">
+ 	
+
+    
+    <div class="card">
+              <img src="avatarImage/blank-avatar.gif" alt="Avatar" style="width:100%">
+              <div class="container">
+                <h4><b></b></h4> 
+                <p>Interior Designer</p> 
+              </div>
+    </div>
+
+
+    
+        <div class="card">
+              <img src="avatarImage/blank-avatar.gif" alt="Avatar" style="width:100%">
+              <div class="container">
+                <h4><b>Jane Doe</b></h4> 
+                <p>Interior Designer</p> 
+              </div>
+    </div>
+    
+ 
+  
+        <div class="card">
+              <img src="avatarImage/blank-avatar.gif" alt="Avatar" style="width:100%">
+              <div class="container">
+                <h4><b>Jane Doe</b></h4> 
+                <p>Interior Designer</p> 
+              </div>
+    </div>
+    
+ 
+    
+    </div>
+ 
+ 
+ 
+ 
+</div>
+
+
+
+
+
+
+
+
+
+<footer class="container-fluid text-center">
+  <p>Footer Text </p>
+</footer>
+
+</body>
+</html>
