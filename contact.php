d83b90f0797f4054eaa38fff055cd8ca0ec85e7e
LuciferDK <chris.kurg@gmail.com>
2017-03-29 01:35:03 +0300

    added ALOT of news shit to webpage
    about.php
    contact
    index changes
    register.php
    main.css changes
    navigation.css changes
    tutorials.php

---
+++ contact.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+  <title>test</title>
+  <link rel="icon" type="image/png" href="image/logoicon.png" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href=   "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="javascript/jquary.js"> </script>
+  <link rel="stylesheet" href="style/main.css" type="text/css">
+  <link rel="stylesheet" href="style/navigation.css" type="text/css">
+
+	
+  
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="navbar-default" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About Us</a></li>
+        <li><a href="tutorials.php">Tutorials</a></li>
+        <li class="active"><a href="contact.php">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="col-sm-2 text-left">
+      <h1>Welcome col 2</h1>
+      <p></p>
+      <hr>
+      <h3></h3>
+      <p></p>
+</div>
+
+<div class="col-sm-10 text-center">
+      <h1>Welcome TEST col 10</h1>
+      <p></p>
+      <hr>
+      <h3></h3>
+      <p></p>
+</div>
+
+
+
+
+
+<footer class="container-fluid text-center">
+  <p>Footer Text </p>
+</footer>
+
+
+
+
+
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,19 +2,109 @@
 <html>
 <head>
 
-	<title>test</title>
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+  <title>test</title>
   <link rel="icon" type="image/png" href="image/logoicon.png" />
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href=   "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="javascript/jquary.js"> </script>
   <link rel="stylesheet" href="style/main.css" type="text/css">
+  <link rel="stylesheet" href="style/navigation.css" type="text/css">
 
-	
+	<style>
+/* Full-width input fields */
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
+
+/* Extra styles for the cancel button */
+.cancelbtn {
+    padding: 14px 20px;
+    background-color: #f44336;
+}
+
+/* Float cancel and signup buttons and add an equal width */
+.cancelbtn,.signupbtn {float:left;width:50%}
+
+/* Add padding to container elements */
+.container {
+    padding: 16px;
+}
+
+/* The Modal (background) */
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+    padding-top: 60px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+    background-color: #fefefe;
+    margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+    border: 1px solid #888;
+    width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button (x) */
+.close {
+    position: absolute;
+    right: 35px;
+    top: 15px;
+    color: #000;
+    font-size: 40px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: red;
+    cursor: pointer;
+}
+
+/* Clear floats */
+.clearfix::after {
+    content: "";
+    clear: both;
+    display: table;
+}
+
+/* Change styles for cancel button and signup button on extra small screens */
+@media screen and (max-width: 300px) {
+    .cancelbtn, .signupbtn {
+       width: 100%;
+    }
+}
+</style>
   
 </head>
 <body>
@@ -28,27 +118,47 @@
     </div>
     <div class="navbar-default" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">LINK</a></li>
-        <li><a href="#">LINK</a></li>
-        <li><a href="#">LINK</a></li>
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="about.php">About Us</a></li>
+        <li><a href="tutorials.php">Tutorials</a></li>
+        <li><a href="contact.php">Contact</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
       <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <li><a href="#" onclick="document.getElementById('id01').style.display='block'"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <button class="glyphicon glyphicon-log-in"  onclick="document.getElementById('id01').style.display='block'">Sign Up</button>
       </ul>
     </div>
   </div>
 </nav>
 
 
-<div class="col-sm-10 text-center">
-      <h1>Welcome TEST col 10</h1>
-      <p></p>
-      <hr>
-      <h3></h3>
-      <p></p>
+<div id="id01" class="modal">
+  <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">×</span>
+  <form class="modal-content animate" action="/action_page.php">
+    <div class="container">
+      <label><b>Email</b></label>
+      <input type="text" placeholder="Enter Email" name="email" required>
+
+      <label><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+
+      <label><b>Repeat Password</b></label>
+      <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+      <input type="checkbox" checked="checked"> Remember me
+      <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+
+      <div class="clearfix">
+        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+        <button type="submit" class="signupbtn">Sign Up</button>
+      </div>
+    </div>
+  </form>
 </div>
+
+
+
+
 
 
 <div class="col-sm-2 text-left">
@@ -59,6 +169,18 @@
       <p></p>
 </div>
 
+<div class="col-sm-10 text-center">
+      <h1>Welcome TEST col 10</h1>
+      <p></p>
+      <hr>
+      <h3></h3>
+      <p></p>
+</div>
+
+
+
+
+
 <footer class="container-fluid text-center">
   <p>Footer Text </p>
 </footer>
@@ -67,6 +189,16 @@
 
 
 
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
 </body>
 </html>
